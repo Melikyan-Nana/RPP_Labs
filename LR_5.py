0a3be3f50bd1c7374ec17c166e1de55d5aaccc20
Melikyan-Nana <114040581+Melikyan-Nana@users.noreply.github.com>
2023-04-21 23:05:03 +0700

    [Лабораторная 5]: Добавили код с выполненными заданиями

--- LR_5.py
+++ LR_5.py
@@ -1,50 +1,54 @@
-import sqlite3
 import pandas as pd
+import sqlite3
 
-def create_function():
+
+def create_table_users():
     conn = sqlite3.connect('users')
     cursor = conn.cursor()
     cursor.execute("""CREATE TABLE IF NOT EXISTS users (id INTEGER NOT NULL,
-                   name varchar (100),
-                   email varchar (100),
+                   name varchar(100),
+                   email varchar(100),
                    PRIMARY KEY (id));""")
     conn.commit()
     conn.close()
 
 
-def insert_function(id, name, email):
+def add_user(id, name, email):
     conn = sqlite3.connect('users')
     cursor = conn.cursor()
-    cursor.execute("""insert into users (id, name, email) VALUES  (:id, :name, :email) """, {"id":id, "name":name, "email":email})
+    cursor.execute("""insert into users (id, name, email) VALUES  (:id, :name, :email) """, {"id": id, "name": name, "email": email})
     conn.commit()
     conn.close()
 
 
-def select_function():
+def select_users():
     conn = sqlite3.connect('users')
     cursor = conn.cursor()
     cursor.execute("""select * from users """)
-    df = pd.DataFrame(cursor.fetchall(), columns= ['id', 'name', 'email'])
-    print (df)
+    df = pd.DataFrame(cursor.fetchall(), columns=['id', 'name', 'email'])
+    print(df)
     conn.commit()
     conn.close()
 
-def select1_function(id):
+
+def select_user_id(id):
     conn = sqlite3.connect('users')
     cursor = conn.cursor()
     cursor.execute("""select * from users
-                        where id=:id""", {"id":id})
+                        where id=:id""", {"id": id})
     print(cursor.fetchone())
     conn.commit()
     conn.close()
 
-def delete_function(id):
+
+def delete_user_by_id(id):
     conn = sqlite3.connect('users')
     cursor = conn.cursor()
     cursor.execute("""delete  from users
-                        where id=:id""", {"id":id})
+                        where id=:id""", {"id": id})
     conn.commit()
     conn.close()
+
 
 def drop_function():
     conn = sqlite3.connect('users')
@@ -53,16 +57,17 @@ def drop_function():
     conn.commit()
     conn.close()
 
-	def main():
-    create_function()
-    insert_function(1, 'Нахшун', 'Nana@mail.ru')
-    insert_function(2, 'Кристина', 'Kris@mail.ru')
-    insert_function(3, 'Нахшун', 'Nana@mail.ru')
-    insert_function(4, 'Кристина', 'Kris@mail.ru')
-    select_function()
-    select1_function(3)
-    delete_function(3)
-    select_function()
+
+def main():
+    create_users()
+    add_user(1, 'Нана', 'Nana@mail.ru')
+    add_user(2, 'Кристина', 'Kris@mail.ru')
+    add_user(3, 'Нана', 'Nana2002@mail.ru')
+    add_user(4, 'Кристина', 'Kristina2002@mail.ru')
+    select_users()
+    select_user_by_id(4)
+    delete_user_by_id(4)
+    select_users()
     # drop_function()
 
 main()
